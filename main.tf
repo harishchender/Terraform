215fb62d96c4c6b4062f79721617d6f134e0136d
root <root@ip-172-31-31-97.ec2.internal>
2024-06-13 06:06:49 +0000

    myfile

--- main.tf
+++ main.tf
@@ -4,10 +4,7 @@ provider "aws" {
   secret_key = "PlAeRC6MsZ7QVreA2bHgHvp12jS9VbP+6JsPakTC"
 }
 
-data "aws_s3_bucket" "mybucket" {
+resource "aws_s3_bucket" "mybucket" {
    bucket = "harish-bucket"
 }
 
-output "my_bucket_name" {
-   value = "${data.aws_s3_bucket.mybucket.bucket}"
-}
